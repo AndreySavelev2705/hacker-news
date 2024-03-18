1484f2a8fcaf8bc0ace7562625f0e64ec6bcb442
Andrey Svelev <savelev.andrey.job.mail@gmail.com>
2024-03-18 21:19:56 +0300

    Added hacker-news api

--- src/App.js
+++ src/App.js
@@ -1,68 +1,33 @@
 import {NewsItem} from "./NewsItem/NewsItem";
 import {useEffect, useState} from "react";
-
-const initNews = [
-    {
-        title: 'Первая новость',
-        url: 'www.example.com',
-        username: 'пользователь',
-        date: '11.11.11',
-        score: 100,
-        id: '1'
-    },
-    {
-        title: 'Вторая новость',
-        url: 'www.example.com',
-        username: 'пользователь 2',
-        date: '11.10.11',
-        score: 1000,
-        id: '2'
-    },
-    {
-        title: 'Третья новость',
-        url: 'www.example.com',
-        username: 'пользователь 3',
-        date: '11.09.11',
-        score: 200,
-        id: '3'
-    }
-]
-
-const newNews = {
-    title: 'Четвертая новость',
-    url: 'www.example.com',
-    username: 'пользователь 3',
-    date: '11.09.11',
-    score: 100,
-    id: '4'
-}
+import {get} from "./api/api";
 
 function App() {
-    const checkStorage = () => JSON.stringify(window.localStorage.getItem('newsKey')) || initNews
-    const [news, setNews] = useState(checkStorage)
-
-
-
-    const newCountHandler = () => {
-        setNews((prevState) => [...prevState, newNews])
-    }
+    const [news, setNews] = useState([])
 
     useEffect(() => {
-        window.localStorage.setItem('newsKey', JSON.stringify(news))
-    }, [news]);
+        getNewsList()
+    }, [])
+
+    async function getNewsList() {
+        const newsIds = await get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty&orderBy="$priority"&limitToFirst=10')
+        const newsList = await Promise
+            .all(newsIds.map((id) => get(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`)))
+
+        setNews(newsList)
+    }
 
     return (
         <>
             <div>Количество новостей: {news.length}</div>
-            <button onClick={newCountHandler}>Добавить новость</button>
             {
                 news.map(item => {
                     return <NewsItem
                         key={item.id}
                         title={item.title}
                         url={item.url}
-                        username={item.username}
-                        date={item.date}
+                        username={item.by}
+                        date={item.time}
                         score={item.score}/>
                 })
             }
--- src/NewsItem/NewsItem.js
+++ src/NewsItem/NewsItem.js
@@ -1,6 +1,18 @@
 import style from './NewsItem.module.css'
+import {unixToDate} from "../utils/Utils";
 
 export function NewsItem(props) {
+    // Вынести бы в отдельную функцию возвращение стиля
+    const scoreClassArr = [style.score]
+
+    if (props.score > 50) {
+        scoreClassArr.push(style.highScore)
+    } else if (props.score > 30) {
+        scoreClassArr.push(style.midScore)
+    } else {
+        scoreClassArr.push(style.lowScore)
+    }
+
     return (
         <div className={style.container}>
             <a className={style.link} href="example.com">{props.title}</a>
@@ -8,10 +20,10 @@ export function NewsItem(props) {
             <div className={style.info}>
                 <div className={style.userData}>
                     <span>{props.username} | </span>
-                    <span>{props.date}</span>
+                    <span>{unixToDate(props.date)}</span>
                 </div>
 
-                <div className={style.score}>
+                <div className={scoreClassArr.join(' ')}>
                     {props.score} points
                 </div>
             </div>
